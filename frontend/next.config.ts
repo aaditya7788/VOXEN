aa622f2b226a9d603b991356d3148fb4d20dcdf2
aaditya7788 <aadityasahani078@gmail.com>
2026-02-16 13:51:47 +0530

    updated next js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -14,6 +14,11 @@ const nextConfig: NextConfig = {
   },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@react-native-async-storage/async-storage": false,
+    };
     return config;
   },
   images: {
